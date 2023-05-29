e5ec26facfb20456b39b5c23b5d404e517c582e8
Bertan ULUSOY <bertan_ulusoy@yahoo.com>
2023-05-29 14:57:47 +0300

    Week 2, Homework, 4th question answered.

--- week_2/hpo.py
+++ week_2/hpo.py
@@ -47,6 +47,7 @@ def run_optimization(data_path: str, num_trials: int):
         rf.fit(X_train, y_train)
         y_pred = rf.predict(X_val)
         rmse = mean_squared_error(y_val, y_pred, squared=False)
+        mlflow.log_metric("rmse", rmse)
 
         return rmse
 
