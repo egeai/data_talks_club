baecab5cefe2954d1aa533f0eccb191637d815e9
Bertan ULUSOY <bertan_ulusoy@yahoo.com>
2023-05-29 14:14:05 +0300

    Week 2, Homework, 3th question answered.

--- week_2/train.py
+++ week_2/train.py
@@ -1,9 +1,14 @@
 import os
 import pickle
 import click
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("nyc_taxi_experiment")
+mlflow.autolog()  # Or other tracking functions
 
 
 def load_pickle(filename: str):
@@ -19,14 +24,23 @@ def load_pickle(filename: str):
 )
 def run_train(data_path: str):
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    with mlflow.start_run() as run:
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+        mlflow.set_tag("developer", "bertan")
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+        max_depth_param = 10
+        mlflow.log_param("max_depth", max_depth_param)
+
+        rf = RandomForestRegressor(max_depth=max_depth_param, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+
+        mlflow.log_metric("rmse", rmse)
 
 
 if __name__ == '__main__':
